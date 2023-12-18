49b5d82f79d05b99e65c76d588bb86c76deacc88
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-18 22:15:24 +0900

    Control input value

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import {
 
 import appStylesHref from "./app.css";
 import {createEmptyContact, getContacts} from "./data";
+import {useEffect} from "react";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
@@ -23,7 +24,7 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return json({ contacts });
+  return json({ contacts, q });
 };
 
 export const action = async () => {
@@ -32,8 +33,15 @@ export const action = async () => {
 };
 
 export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
+  const { contacts, q } = useLoaderData<typeof loader>();
   const navigation = useNavigation()
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <html lang="en">
@@ -51,6 +59,7 @@ export default function App() {
             <input
               id="q"
               aria-label="Search contacts"
+              defaultValue={q || ""}
               placeholder="Search"
               type="search"
               name="q"
