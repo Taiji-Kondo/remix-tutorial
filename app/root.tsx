11d0eee35922936283f73917c60ac5fb2f399093
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-18 21:05:03 +0900

    Use NavLink

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import {
   Link,
   Links,
   LiveReload,
-  Meta,
+  Meta, NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
@@ -63,7 +63,16 @@ export default function App() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                          ? "pending"
+                          : ""
+                    }
+                    to={`contacts/${contact.id}`}
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -74,7 +83,7 @@ export default function App() {
                     {contact.favorite ? (
                       <span>★</span>
                     ) : null}
-                  </Link>
+                  </NavLink>
                 </li>
               ))}
             </ul>
