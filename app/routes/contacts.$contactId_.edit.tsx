bf46bcc7b1cb3ebf95e30c46ea023df6dbb30282
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-18 20:23:22 +0900

    Add form action

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,19 +1,25 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import {ActionFunctionArgs, LoaderFunctionArgs, redirect} from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
-import { getContact } from "~/data";
+import {getContact, updateContact} from "~/data";
 
-export const loader = async ({
-                               params,
-                             }: LoaderFunctionArgs) => {
+export const loader = async ({params}: LoaderFunctionArgs) => {
   invariant(params.contactId, "Missing contactId param");
   const contact = await getContact(params.contactId);
   if (!contact) {
     throw new Response("Not Found", { status: 404 });
   }
   return json({ contact });
+};
+
+export const action = async ({params, request}: ActionFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
 };
 
 export default function EditContact() {
