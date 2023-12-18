22c81d4eb2432d478d817a990de24cb0b1b51d72
Taiji-Kondo <k.taiji1108@gmail.com>
2023-12-18 21:14:53 +0900

    Add global pending ui

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,6 @@ import {json, LinksFunction, redirect} from "@remix-run/node";
 
 import {
   Form,
-  Link,
   Links,
   LiveReload,
   Meta, NavLink,
@@ -10,6 +9,7 @@ import {
   Scripts,
   ScrollRestoration,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css";
@@ -31,6 +31,7 @@ export const action = async () => {
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation()
 
   return (
     <html lang="en">
@@ -95,7 +96,12 @@ export default function App() {
         </nav>
       </div>
 
-      <div id="detail">
+      <div
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+        id="detail"
+      >
         <Outlet/>
       </div>
 
